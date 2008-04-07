0aa45abaca7af88c971b43ed284cc8e90fd15859
cmsbuild <>
2008-04-07 17:05:56 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- TwoBodyDecay-V00-01-07-01'.
    Sprout from master 2007-12-06 01:58:43 UTC Natalia Ratnikova <sha1-eabc1741ce19fdddc972ebf366f29d441dd8c670@cern.ch> 'QA campaign: includes cleanup based on includechecker results.'
    Cherrypick from master 2008-04-07 17:05:55 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'remove typedef for backport to 16X':
        interface/TwoBodyDecayFitter.h
        src/TwoBodyDecayFitter.cc

--- src/TwoBodyDecayFitter.cc
+++ src/TwoBodyDecayFitter.cc
@@ -25,23 +25,40 @@ TwoBodyDecayFitter::~TwoBodyDecayFitter( void ) {}
 const TwoBodyDecay TwoBodyDecayFitter::estimate( const vector< reco::TransientTrack >& tracks,
 						 const TwoBodyDecayVirtualMeasurement& vm ) const
 {
-  try
-  {
-    // get geometrical linearization point
-    GlobalPoint linVertex = theVertexFinder->getLinearizationPoint( tracks );
+  // get geometrical linearization point
+  GlobalPoint linVertex = theVertexFinder->getLinearizationPoint( tracks );
 
-    // create linearized track states
-    vector< RefCountedLinearizedTrackState > linTracks;
-    linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[0] ) );
-    linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[1] ) );
+  // create linearized track states
+  vector< RefCountedLinearizedTrackState > linTracks;
+  linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[0] ) );
+  linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[1] ) );
 
-    // get full linearization point (geomatrical & kinematical)
-    const TwoBodyDecayParameters linPoint = theLinPointFinder->getLinearizationPoint( linTracks, vm.primaryMass(), vm.secondaryMass() );
+  // get full linearization point (geomatrical & kinematical)
+  const TwoBodyDecayParameters linPoint = theLinPointFinder->getLinearizationPoint( linTracks, vm.primaryMass(), vm.secondaryMass() );
 
-    // make the fit
-    return theEstimator->estimate( linTracks, linPoint, vm );
-  }
-  catch (...) {}
+  // make the fit
+  return theEstimator->estimate( linTracks, linPoint, vm );
+}
 
-  return TwoBodyDecay();
+
+const TwoBodyDecay TwoBodyDecayFitter::estimate( const vector< reco::TransientTrack >& tracks,
+						 const std::vector< TrajectoryStateOnSurface >& tsos,
+						 const TwoBodyDecayVirtualMeasurement& vm ) const
+{
+  // get geometrical linearization point
+  std::vector< FreeTrajectoryState > freeTrajStates;
+  freeTrajStates.push_back( *tsos[0].freeState() );
+  freeTrajStates.push_back( *tsos[1].freeState() );
+  GlobalPoint linVertex = theVertexFinder->getLinearizationPoint( freeTrajStates );
+
+  // create linearized track states
+  vector< RefCountedLinearizedTrackState > linTracks;
+  linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[0], tsos[0] ) );
+  linTracks.push_back( theLinTrackStateFactory.linearizedTrackState( linVertex, tracks[1], tsos[1] ) );
+
+  // get full linearization point (geomatrical & kinematical)
+  const TwoBodyDecayParameters linPoint = theLinPointFinder->getLinearizationPoint( linTracks, vm.primaryMass(), vm.secondaryMass() );
+
+  // make the fit
+  return theEstimator->estimate( linTracks, linPoint, vm );
 }
